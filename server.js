0edb1e9aa283a63053816694612b868662371a68
sunnyraindev <ruairiletzer@outlook.com>
2024-09-12 22:46:28 -0400

    add online user counter

--- server.js
+++ server.js
@@ -8,37 +8,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const mongoURI = process.env.MONGO_URI
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI);
 const db = mongoose.connection;
 
-// Handle MongoDB connection errors
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// Define a schema for chat messages
 const messageSchema = new mongoose.Schema({
     message: String,
     screenName: String,
     timestamp: { type: Date, default: Date.now }
 });
 
-// Create a model based on the schema
 const Message = mongoose.model('Message', messageSchema);
 
-// Serve static files
 app.use(express.static('public'));
 
-// Handle Socket.IO connections
+let onlineUsers = 0;
+
 io.on('connection', (socket) => {
-    let userIP = socket.request.connection.remoteAddress;
+    onlineUsers++;
+    io.emit('onlineUsers', onlineUsers);
 
-    if (socket.handshake.headers['x-forwarded-for']) {
-        userIP = socket.handshake.headers['x-forwarded-for'].split(',')[0];
-    }
+    console.log(`A user connected. Total online users: ${onlineUsers}`);
 
-    console.log(`New user connected from ${userIP}`);
-
-    // Load chat history from MongoDB and send to the user
     const loadChatHistory = async () => {
         try {
             const messages = await Message.find().sort({ timestamp: 1 }).exec();
@@ -50,7 +43,6 @@ io.on('connection', (socket) => {
 
     loadChatHistory();
 
-    // Handle incoming messages
     socket.on('message', async (data) => {
         const { message, screenName } = data;
         const newMessage = new Message({ message, screenName });
@@ -59,10 +51,8 @@ io.on('connection', (socket) => {
             await newMessage.save();
             io.emit('message', newMessage);
 
-            // Check if message count exceeds 500
             const messageCount = await Message.countDocuments();
             if (messageCount > 500) {
-                // Find and delete the oldest message(s)
                 const oldestMessages = await Message.find().sort({ timestamp: 1 }).limit(messageCount - 500);
                 for (let msg of oldestMessages) {
                     await msg.remove();
@@ -74,7 +64,10 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        onlineUsers--;
+        io.emit('onlineUsers', onlineUsers);
+
+        console.log(`A user disconnected. Total online users: ${onlineUsers}`);
     });
 });
 
